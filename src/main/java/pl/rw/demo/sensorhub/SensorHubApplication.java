2006f5c628ce3bb4d81f56fa70632725ffb39c3d
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2019-11-29 06:39:27 +0100

    Initial commit

---
+++ src/main/java/pl/rw/demo/sensorhub/SensorHubApplication.java
@@ -0,0 +1,13 @@
+package pl.rw.demo.sensorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rw/demo/sensorhub/SensorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rw.demo.sensorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
